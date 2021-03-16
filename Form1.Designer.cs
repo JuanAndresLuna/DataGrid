00a50eb3278fac7c9359419c7324a05007794cba
Juan Luna <andruluna97@icloud.com>
2021-03-16 10:51:11 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace DataGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGrilla2 = new System.Windows.Forms.Button();
+            this.btnExcel2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGrilla2
+            // 
+            this.btnGrilla2.Location = new System.Drawing.Point(82, 396);
+            this.btnGrilla2.Name = "btnGrilla2";
+            this.btnGrilla2.Size = new System.Drawing.Size(272, 70);
+            this.btnGrilla2.TabIndex = 0;
+            this.btnGrilla2.Text = "LLenar Grilla";
+            this.btnGrilla2.UseVisualStyleBackColor = true;
+            this.btnGrilla2.Click += new System.EventHandler(this.btnGrilla2_Click);
+            // 
+            // btnExcel2
+            // 
+            this.btnExcel2.Location = new System.Drawing.Point(575, 396);
+            this.btnExcel2.Name = "btnExcel2";
+            this.btnExcel2.Size = new System.Drawing.Size(272, 70);
+            this.btnExcel2.TabIndex = 1;
+            this.btnExcel2.Text = "Exportar a Excel";
+            this.btnExcel2.UseVisualStyleBackColor = true;
+            this.btnExcel2.Click += new System.EventHandler(this.btnExcel2_Click_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1005, 342);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1009, 510);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnExcel2);
+            this.Controls.Add(this.btnGrilla2);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView TablaDatos;
+        private System.Windows.Forms.Button btnGrilla;
+        private System.Windows.Forms.Button btnExcel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Titulos;
+        private System.Windows.Forms.Button btnGrilla2;
+        private System.Windows.Forms.Button btnExcel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Office.Interop.Excel;
+using objExcel = Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace DataGrid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGrilla2_Click(object sender, EventArgs e)
+        {
+            Grilla.Negocio ng = new Grilla.Negocio();
+            System.Data.DataTable dt = (System.Data.DataTable) ng.llenarGrilla();//casteo a datatable
+            if(dt != null)
+            {
+                dataGridView1.DataSource = dt;
+            }
+            else MessageBox.Show("No hay datos");
+            
+        }
+        string ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+
+        private void btnExcel2_Click_1(object sender, EventArgs e)
+        {
+            objExcel.Application objAplicacion = new objExcel.Application();
+            Workbook objLibro = objAplicacion.Workbooks.Add(XlSheetType.xlWorksheet);//nuevo libro en archivo excel
+            Worksheet objHoja = (Worksheet)objAplicacion.ActiveSheet;//Hoja de calculo en nuesto nuevo libro
+
+            objAplicacion.Visible = false; // abre automarico el excel (no quiero)
+
+            foreach (DataGridViewColumn columna in dataGridView1.Columns)
+            {
+                objHoja.Cells[1, columna.Index+1] = columna.HeaderText;
+                foreach (DataGridViewRow fila in dataGridView1.Rows)
+                {
+                    objHoja.Cells[fila.Index+2, columna.Index+1] = fila.Cells[columna.Index].Value;
+                }
+            }
+
+            objLibro.SaveAs(ruta + "\\PruebaExcel.xlsx");
+            objLibro.Close();
+            objAplicacion.Quit();
+            MessageBox.Show("Excel Listo!");
+
+        }
+    }
+}
